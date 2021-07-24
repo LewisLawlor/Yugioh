2e173252a410491f5572af5cc1dc0523921f3f3e
ScriptKiddie / <schnarfengargle123@gmail.com>
2021-07-24 18:05:38 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// Create a button called displayDeck
+
+// Create a button called addToDeck
+
+// Create a button called removeFromDeck
+
+// Create an input called searchCard
+
+// Create an input called searchDeck
+
+const selectMonsterButton = document.getElementById("select-monster-button");
+
+function selectMonster() {
+  // function code
+}
+
+selectMonsterButton.onclick = selectMonster;
+
+// Code the same as above for the other buttons and inputs, 
+// and give their html elements an id.
