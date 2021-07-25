b103cf69740fc9638e24e7c5967b9f4136fe7873
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-25 11:17:06 +0100

    Made changes

--- src/app.js
+++ src/app.js
@@ -9,12 +9,29 @@
 // Create an input called searchDeck
 
 const selectMonsterButton = document.getElementById("select-monster-button");
+const displayDeck = document.getElementById("display-deck");
+    const addToDeckButton = document.getElementById("add-to-deck-button");
+    const removeFromDeck = document.getElementById("remove-from-deck");
+    const searchCard = document.getElementById("search-card");
+    const searchDeck = document.getElementById("search-deck");
 
 function selectMonster() {
   // function code
 }
 
+function displayDeck() {
+
+}
+
+function addToDeck () {
+  
+}
+
+
+
 selectMonsterButton.onclick = selectMonster;
+
+addToDeckButton.onclick = addToDeck;
 
 // Code the same as above for the other buttons and inputs, 
 // and give their html elements an id.
